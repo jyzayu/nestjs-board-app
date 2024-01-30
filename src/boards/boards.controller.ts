656c9bb283597eb668236ba27d63acc6725a2abf
JY CHOI <moosou30@naver.com>
2024-01-30 14:57:49 +0900

    feat: add board module CRUD  REST API

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './boards.model';
+import { CreateBoardDto } from './dto/create-board-dto';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoards(@Body() createBoardDto: CreateBoardDto) {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoardById(@Param('id') id: string): void {
+    this.boardsService.deleteBoardById(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+}
